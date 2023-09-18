f8eedac9001c027e169ef852aeb26eb8a3ea985b
Michael Miceli <ilecimleahcim@gmail.com>
2023-09-18 13:26:12 -0600

    add linear model

--- BikeSHareAnalysis.R
+++ BikeSHareAnalysis.R
@@ -3,14 +3,31 @@ library(vroom)
 library(tidyverse)
 library(tidymodels)
 
+test <- vroom("C:/School/Stat348/KaggleBikeShare/test.csv")
 vData <- vroom("C:/School/Stat348/KaggleBikeShare/train.csv")
-
+vData <- vData%>%
+  select(-casual, -registered)
 my_recipe <- recipe(count ~ ., data=vData) %>%
-  step_mutate(season=as.factor(vData$season)) %>%           #change categorical data to factors
-  step_mutate(holiday=as.factor(vData$holiday)) %>%        #change categorical data to factors
-  step_mutate(workingday=as.factor(vData$workingday)) %>%
-  step_mutate(weather= as.factor(vData$weather)) %>%    #change categorical data to factors
-  step_mutate(rental = registered + casual) %>%
+  step_num2factor(season) %>%           
+  step_num2factor(holiday) %>%        
+  step_num2factor(workingday) %>%
+  step_num2factor(weather)%>%
+  step_mutate(weather=ifelse(weather==4,3,weather))%>%
   step_time(datetime, features="hour")
-prepped_recipe <- prep(my_recipe)
-bake(prepped_recipe, new_data=baked_Data)
+prepped_recipe <- prep(my_recipe) %>%
+  juice()
+newData <- bake(prepped_recipe, new_data=vData)
+
+
+
+my_mod <- linear_reg() %>%
+  set_engine("lm")
+
+bike_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod) %>%
+  fit(data=vData)
+
+bike_predictions <- predict(bike_workflow,
+                            new_data=test)
+
